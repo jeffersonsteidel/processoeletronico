33729f80cbf3ec9c5ef58d425ec7cbc161c51701
nicole.u.dias <nicole.u.dias@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-09-28 13:42:55 +0000

    Criação: Classe DadosPessoais, DadosFuncionais e Injetor.

---
+++ ProcessoEletronico/src/br/com/progepe/extract/DadosFuncionais.java
@@ -0,0 +1,53 @@
+//package br.com.progepe.extract;
+//
+//public class DadosFuncionais {
+//
+//	private Integer matriculaSiape;
+//	private Integer tipoRegistro;
+//	private Integer codigoSituacaoServidor;
+//	private Integer numeroCarteiraDeTrabalho;
+//	private Integer serieCarteiraDeTrabalho;
+//	private String UfCarteiraDeTrabalho;
+//	private Integer codigoBanco;
+//	private String agenciaBanco;
+//	private String contaCorrenteBanco;
+//	private Integer dataCadastramentoServidor;
+//	private Integer codGrupo;
+//	private Integer codCargo;
+//	private String classeCargo;
+//	private String referenciaNivelPadraoCargo;
+//	private Integer dataEntradaOcupacaoCargo;
+//	private Integer dataSaidaCargo;
+//	private String siglaFuncao;
+//	private Integer codigoNivelFuncao;
+//	private String codigoEscolaridadeFuncao;
+//	private String codigoOpcaoFuncao;
+//	private Integer dataIngressoFuncao;
+//	private Integer dataSaidaFuncao;
+//	private Integer codigoUnidadeOrganizacionalFuncao;
+//	private String siglaNovaFuncao;
+//	private Integer codigoNivelNovaFuncao;
+//	private String codigoEscolaridadeNovaFuncao;
+//	private String codigoOpcaoNovaFuncao;
+//	private Integer dataIngressoNovaFuncao;
+//	private Integer dataSaidaNovaFuncao;
+//	private Integer codigoUnidadeOrganizacionalNovaFuncao;
+//	private Integer codigoUnidadeOrganizacionalLotacao;
+//	private Integer dataLotacao;
+//	private Integer codigoOrgaoLocalizacao;
+//	private Integer codigoUnidadeOrganizacionalLocalizacao;
+//	private Integer codigoGrupoIngressoOrgao;
+//	private Integer codigoOcorrenciaIngressoOrgao;
+//	private Integer dataIngressoOrgao;
+//	private Integer codigoDiplomaLegalIngressoOrgao;
+//	private String numeroDiplomaLegal;
+//	private Integer dataPublicacaoDiplomaLegal;
+//	private Integer codigoGrupoIngressoServPublico;
+//	private Integer codigoIngressoServPublico;
+//	private Integer dataIngressoServPublico;
+//	private Integer codigoDiplomaLegalOcorrenciaServPublico;
+//	private String numeroDiplomaLegalOcorrenciaServPublico;
+//	private Integer dataPublicacaoDiplomaLegalOcorrenciaServPublico;
+//
+//	
+//}
---
+++ ProcessoEletronico/src/br/com/progepe/extract/DadosPessoais.java
@@ -0,0 +1,276 @@
+package br.com.progepe.extract;
+
+public class DadosPessoais {
+
+	private Integer matriculaSiape;
+	private Integer dvMatricula;
+	private Integer tipoRegistro;
+	private String nomeServidor;
+	private Integer cpf;
+	private Integer pisPasef;
+	private String nomeMae;
+	private String sexo;
+	private Integer dataNascimento;
+	private Integer estadoCivil;
+	private Integer nivelEscolaridade;
+	private Integer codigoTitulacaoFormacao;
+	private Integer nacionalidade;
+	private String naturalidade;
+	private Integer pais;
+	private Integer anoChegada;
+	private Integer dataPrimeiroEmprego;
+	private Integer identificacaoOrigem;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String municipio;
+	private Integer cep;
+	private String uf;
+	private String numeroRegistroGeral;
+	private String siglaOrgaoExpedidor;
+	private Integer dataExpedicaoIdentidade;
+	private String siglaUfIdentidade;
+	private Integer numeroTituloEleitor;
+
+	public Integer getMatriculaSiape() {
+		return matriculaSiape;
+	}
+
+	public void setMatriculaSiape(Integer matriculaSiape) {
+		this.matriculaSiape = matriculaSiape;
+	}
+
+	public Integer getDvMatricula() {
+		return dvMatricula;
+	}
+
+	public void setDvMatricula(Integer dvMatricula) {
+		this.dvMatricula = dvMatricula;
+	}
+
+	public Integer getTipoRegistro() {
+		return tipoRegistro;
+	}
+
+	public void setTipoRegistro(Integer tipoRegistro) {
+		this.tipoRegistro = tipoRegistro;
+	}
+
+	public String getNomeServidor() {
+		return nomeServidor;
+	}
+
+	public void setNomeServidor(String nomeServidor) {
+		this.nomeServidor = nomeServidor;
+	}
+
+	public Integer getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+
+	public Integer getPisPasef() {
+		return pisPasef;
+	}
+
+	public void setPisPasef(Integer pisPasef) {
+		this.pisPasef = pisPasef;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public Integer getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Integer dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Integer getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(Integer estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public Integer getNivelEscolaridade() {
+		return nivelEscolaridade;
+	}
+
+	public void setNivelEscolaridade(Integer nivelEscolaridade) {
+		this.nivelEscolaridade = nivelEscolaridade;
+	}
+
+	public Integer getCodigoTitulacaoFormacao() {
+		return codigoTitulacaoFormacao;
+	}
+
+	public void setCodigoTitulacaoFormacao(Integer codigoTitulacaoFormacao) {
+		this.codigoTitulacaoFormacao = codigoTitulacaoFormacao;
+	}
+
+	public Integer getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(Integer nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getNaturalidade() {
+		return naturalidade;
+	}
+
+	public void setNaturalidade(String naturalidade) {
+		this.naturalidade = naturalidade;
+	}
+
+	public Integer getPais() {
+		return pais;
+	}
+
+	public void setPais(Integer pais) {
+		this.pais = pais;
+	}
+
+	public Integer getAnoChegada() {
+		return anoChegada;
+	}
+
+	public void setAnoChegada(Integer anoChegada) {
+		this.anoChegada = anoChegada;
+	}
+
+	public Integer getDataPrimeiroEmprego() {
+		return dataPrimeiroEmprego;
+	}
+
+	public void setDataPrimeiroEmprego(Integer dataPrimeiroEmprego) {
+		this.dataPrimeiroEmprego = dataPrimeiroEmprego;
+	}
+
+	public Integer getIdentificacaoOrigem() {
+		return identificacaoOrigem;
+	}
+
+	public void setIdentificacaoOrigem(Integer identificacaoOrigem) {
+		this.identificacaoOrigem = identificacaoOrigem;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getMunicipio() {
+		return municipio;
+	}
+
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+
+	public Integer getCep() {
+		return cep;
+	}
+
+	public void setCep(Integer cep) {
+		this.cep = cep;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getNumeroRegistroGeral() {
+		return numeroRegistroGeral;
+	}
+
+	public void setNumeroRegistroGeral(String numeroRegistroGeral) {
+		this.numeroRegistroGeral = numeroRegistroGeral;
+	}
+
+	public String getSiglaOrgaoExpedidor() {
+		return siglaOrgaoExpedidor;
+	}
+
+	public void setSiglaOrgaoExpedidor(String siglaOrgaoExpedidor) {
+		this.siglaOrgaoExpedidor = siglaOrgaoExpedidor;
+	}
+
+	public Integer getDataExpedicaoIdentidade() {
+		return dataExpedicaoIdentidade;
+	}
+
+	public void setDataExpedicaoIdentidade(Integer dataExpedicaoIdentidade) {
+		this.dataExpedicaoIdentidade = dataExpedicaoIdentidade;
+	}
+
+	public String getSiglaUfIdentidade() {
+		return siglaUfIdentidade;
+	}
+
+	public void setSiglaUfIdentidade(String siglaUfIdentidade) {
+		this.siglaUfIdentidade = siglaUfIdentidade;
+	}
+
+	public Integer getNumeroTituloEleitor() {
+		return numeroTituloEleitor;
+	}
+
+	public void setNumeroTituloEleitor(Integer numeroTituloEleitor) {
+		this.numeroTituloEleitor = numeroTituloEleitor;
+	}
+
+}
