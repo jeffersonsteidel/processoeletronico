cb0a380ef1cf27daf32adf550de3e447feca6d75
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-09-30 17:11:56 +0000

    #jefferson

--- trunk/ProcessoEletronico/src/br/com/progepe/extract/Injetor.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/extract/Injetor.java
@@ -2,14 +2,31 @@ package br.com.progepe.extract;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.HashSet;
 
+import br.com.progepe.dao.CidadeDAO;
 import br.com.progepe.dao.DAO;
+import br.com.progepe.entity.Banco;
+import br.com.progepe.entity.Cargo;
 import br.com.progepe.entity.Cidade;
+import br.com.progepe.entity.Classe;
+import br.com.progepe.entity.Conjuge;
+import br.com.progepe.entity.ContaBancaria;
+import br.com.progepe.entity.CorPele;
+import br.com.progepe.entity.Dependente;
 import br.com.progepe.entity.Documento;
 import br.com.progepe.entity.Endereco;
 import br.com.progepe.entity.Estado;
 import br.com.progepe.entity.EstadoCivil;
+import br.com.progepe.entity.Funcao;
+import br.com.progepe.entity.Grupo;
+import br.com.progepe.entity.GrupoSanguineo;
+import br.com.progepe.entity.Lotacao;
+import br.com.progepe.entity.Padrao;
+import br.com.progepe.entity.RegimeTrabalho;
 import br.com.progepe.entity.Servidor;
+import br.com.progepe.entity.SituacaoFuncional;
+import br.com.progepe.entity.TipoFuncao;
 import br.com.progepe.validator.Validator;
 
 public class Injetor {
@@ -19,17 +36,45 @@ public class Injetor {
 		Extrator injetorDados = new Extrator();
 		injetorDados.carregarDados();
 		DAO dao = new DAO();
+		CidadeDAO cidadeDAO = new CidadeDAO();
 		
 		for (DadosPessoais item : injetorDados.getDadosPessoaisList()) {
 			Servidor servidor = new Servidor();
+			servidor.setCargo(new Cargo());
+			servidor.getCargo().setClasse(new Classe());
+			servidor.setCidadeNascimento(new Cidade());
+			servidor.getCidadeNascimento().setEstado(new Estado());
+			servidor.setConjuge(new Conjuge());
+			servidor.getConjuge().setDocumento(new Documento());
+			servidor.getConjuge().getDocumento().setCarteiraUf(new Estado());
+			servidor.getConjuge().getDocumento().setRgUf(new Estado());
+			servidor.getConjuge().getDocumento().setTituloUf(new Estado());
+			servidor.getConjuge().setCidadeNascimento(new Cidade());
+			servidor.getConjuge().getCidadeNascimento().setEstado(new Estado());
+			servidor.setContaBancaria(new ContaBancaria());
+			servidor.getContaBancaria().setBanco(new Banco());
+			servidor.setCorPele(new CorPele());
 			servidor.setDocumento(new Documento());
 			servidor.getDocumento().setCarteiraUf(new Estado());
 			servidor.getDocumento().setRgUf(new Estado());
 			servidor.getDocumento().setTituloUf(new Estado());
-			servidor.setEstadoCivil(new EstadoCivil());
+			servidor.setDependentes(new HashSet<Dependente>());
 			servidor.setEndereco(new Endereco());
 			servidor.getEndereco().setCidade(new Cidade());
 			servidor.getEndereco().getCidade().setEstado(new Estado());
+			servidor.setEstadoCivil(new EstadoCivil());
+			servidor.setFuncao(new Funcao());
+			servidor.getFuncao().setTipoFuncao(new TipoFuncao());
+			servidor.setGrupo(new Grupo());
+			servidor.setGrupoSanguineo(new GrupoSanguineo());
+			servidor.setLotacao(new Lotacao());
+			servidor.setLocalExercicio(new Lotacao());
+			servidor.setEndereco(new Endereco());
+			servidor.getEndereco().setCidade(new Cidade());
+			servidor.getEndereco().getCidade().setEstado(new Estado());
+			servidor.setPadrao(new Padrao());
+			servidor.setRegimeTrabalho(new RegimeTrabalho());
+			servidor.setSituacaoFuncional(new SituacaoFuncional());
 			
 			servidor.setSiape(item.getMatriculaSiape());
 			servidor.setIdentificacaoUnica(item.getMatriculaSiape() + "-" + item.getDvMatricula());
@@ -46,11 +91,13 @@ public class Injetor {
 			servidor.getEndereco().setComplemento(item.getComplementoEndereco());
 			servidor.getEndereco().setBairro(item.getBairro());
 			servidor.getEndereco().setCep(Validator.formatarCep(item.getCep()));
+			Cidade cidade = cidadeDAO.listByNome(item.getMunicipio().toUpperCase().trim());
+			servidor.getEndereco().setCidade(cidade);
+			servidor.getEndereco().getCidade().setEstado(cidade.getEstado());
 			servidor.getDocumento().setRg(item.getNumeroRegistroGeral());
 			servidor.getDocumento().setRgOrgaoEmissor(item.getSiglaOrgaoExpedidor());
 			servidor.getDocumento().setRgDataExpedicao(new Date(Validator.formatarData(item.getDataExpedicaoIdentidade())));
 			
-		
 			dao.save(servidor);
 			
 			
