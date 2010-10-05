b1e4beb731211bb7ef30e8371d60370307b19bc7
nicole.u.dias <nicole.u.dias@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-05 14:08:33 +0000

    Remoção das classes 'DadosPessoais' e 'DadosFuncionais' --> Substituido por  'DadosFitaEspelho' Alterações: Classes Extrator, Relatorio, RelatorioAltenticacao, Extrator e Injetor

--- trunk/ProcessoEletronico/src/RelatorioAltenticacao.java
+++ trunk/ProcessoEletronico/src/RelatorioAltenticacao.java
@@ -1,33 +1,33 @@
-import java.util.List;
-
-import br.com.progepe.extract.DadosFuncionais;
-import br.com.progepe.extract.DadosPessoais;
-import br.com.progepe.extract.Extrator;
-import br.com.progepe.validator.Validator;
-
-public class RelatorioAltenticacao {
-
-	public static void main(String[] args) throws Exception {
-		List<DadosPessoais> dadosPessoais;
-		List<DadosFuncionais> dadosFuncionais;
-		Extrator extrator = new Extrator();
-		extrator.carregarDados();
-		String data;
-		dadosPessoais = extrator.getDadosPessoaisList();
-		dadosFuncionais = extrator.getDadosFuncionaisList();
-		try {
-			for (int count = 0; count < dadosPessoais.size(); count++) {
-
-				DadosPessoais servidorPessoal = dadosPessoais.get(count);
-				System.out.print(servidorPessoal.getMatriculaSiape());
-				System.out.print("," + servidorPessoal.getNomeServidor());
-				System.out.println("," + servidorPessoal
-						.getDataNascimento().toString());
-
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-
-		}
-	}
-}
+//import java.util.List;
+//
+//import br.com.progepe.extract.DadosFitaEspelho;
+//import br.com.progepe.extract.DadosPessoais;
+//import br.com.progepe.extract.Extrator;
+//import br.com.progepe.validator.Validator;
+//
+//public class RelatorioAltenticacao {
+//
+//	public static void main(String[] args) throws Exception {
+//		List<DadosPessoais> dadosPessoais;
+//		List<DadosFitaEspelho> dadosFitaEspelho;
+//		Extrator extrator = new Extrator();
+//		extrator.carregarDados();
+//		String data;
+//		dadosPessoais = extrator.getDadosPessoaisList();
+//		dadosFitaEspelho = extrator.getDadosFuncionaisList();
+//		try {
+//			for (int count = 0; count < dadosPessoais.size(); count++) {
+//
+//				DadosPessoais servidorPessoal = dadosPessoais.get(count);
+//				System.out.print(servidorPessoal.getMatriculaSiape());
+//				System.out.print("," + servidorPessoal.getNomeServidor());
+//				System.out.println("," + servidorPessoal
+//						.getDataNascimento().toString());
+//
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//
+//		}
+//	}
+//}
