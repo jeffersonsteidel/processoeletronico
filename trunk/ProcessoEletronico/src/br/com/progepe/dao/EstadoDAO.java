5052b5cdb770ad43933e2a81fbf0fc2ae78f8161
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-07 16:47:08 +0000

    ALTERAÇÕES HIBERNATE

--- trunk/ProcessoEletronico/src/br/com/progepe/dao/EstadoDAO.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/dao/EstadoDAO.java
@@ -1,24 +1,14 @@
 package br.com.progepe.dao;
 
-import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
 import br.com.progepe.entity.Estado;
 
 public class EstadoDAO {
 
-	private Session session;
-
-	public EstadoDAO() {
-		session = HibernateUtility.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-	}
-
 	public Estado listByUf(String ufEstado) {
-		Session session = HibernateUtility.getSessionFactory()
-				.getCurrentSession();
-		session.beginTransaction();
-		return (Estado) session.createCriteria(Estado.class).add(
+		HibernateUtility.getSession().beginTransaction();
+		return (Estado) HibernateUtility.getSession().createCriteria(Estado.class).add(
 				Restrictions.like("uf", ufEstado)).uniqueResult();
 	}
 }
--- trunk/ProcessoEletronico/src/br/com/progepe/dao/HibernateUtility.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/dao/HibernateUtility.java
@@ -1,22 +1,68 @@
 package br.com.progepe.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtility {
-	private static final SessionFactory sessionFactory = buildSessionFactory();
-
-	private static SessionFactory buildSessionFactory() {
+	private static final SessionFactory factory;
+	@SuppressWarnings("rawtypes")
+	private static final ThreadLocal sessionThread = new ThreadLocal();
+	@SuppressWarnings("rawtypes")
+	private static final ThreadLocal transactionThread = new ThreadLocal();
+	static {
+		// Bloco estático que inicializa o Hibernate, escreve o stack trace se
+		// houver algum problema e relança a exceção
 		try {
-
-			return new Configuration().configure().buildSessionFactory();
-		} catch (Throwable ex) {
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			throw e;
 		}
 	}
 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
+	@SuppressWarnings("unchecked")
+	public static Session getSession() {
+		if (sessionThread.get() == null) {
+			Session session = factory.openSession();
+			sessionThread.set(session);
+		}
+		return (Session) sessionThread.get();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void closeSession() {
+		Session session = (Session) sessionThread.get();
+		if (session != null && session.isOpen()) {
+			sessionThread.set(null);
+			session.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void beginTransaction() {
+		Transaction transaction = getSession().beginTransaction();
+		transactionThread.set(transaction);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void commitTransaction() {
+		Transaction transaction = (Transaction) transactionThread.get();
+		if (transaction != null && !transaction.wasCommitted()
+				&& !transaction.wasRolledBack()) {
+			transaction.commit();
+			transactionThread.set(null);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static void rollbackTransaction() {
+		Transaction transaction = (Transaction) transactionThread.get();
+		if (transaction != null && !transaction.wasCommitted()
+				&& !transaction.wasRolledBack()) {
+			transaction.rollback();
+			transactionThread.set(null);
+		}
 	}
 }
