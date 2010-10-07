18be17dbaf9864b2517039b59733de0ef66ef06b
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-07 20:35:08 +0000

    #jefferson

--- ProcessoEletronico/src/br/com/progepe/validator/Validator.java
+++ ProcessoEletronico/src/br/com/progepe/validator/Validator.java
@@ -23,22 +23,31 @@ public abstract class Validator {
 			return true;
 	}
 
-	public static boolean validarCNPJCPF(String CNPJ_CPF) {
-		CNPJ_CPF = CNPJ_CPF.replace("-", "").replace(".", "").replace("/", "");
+	public static boolean validarCNPJCPF(final String CNPJ_CPF) {
+
+		CNPJ_CPF.replace(".", "");
+		CNPJ_CPF.replace("-", "");
+		CNPJ_CPF.replace("/", "");
 		if (CNPJ_CPF.length() == 11) { // CPF
+
 			int acumulador1 = 0;
 			int acumulador2 = 0;
 			int resto = 0;
+
 			StringBuffer resultado = new StringBuffer();
+
 			String digitoVerificadorCPF = CNPJ_CPF.substring(
 					(CNPJ_CPF.length() - 2), CNPJ_CPF.length());
+
 			for (int i = 1; i < (CNPJ_CPF.length() - 1); i++) {
 				acumulador1 += (11 - i)
 						* Integer.valueOf(CNPJ_CPF.substring((i - 1), i));
 				acumulador2 += (12 - i)
 						* Integer.valueOf(CNPJ_CPF.substring((i - 1), i));
 			}
+
 			resto = acumulador1 % 11;
+
 			if (resto < 2) {
 				acumulador2 += 2;
 				resultado.append(2);
@@ -46,50 +55,66 @@ public abstract class Validator {
 				acumulador2 += 2 * (11 - resto);
 				resultado.append((11 - resto));
 			}
+
 			resto = acumulador2 % 11;
+
 			if (resto < 2) {
 				resultado.append(0);
 			} else {
 				resultado.append((11 - resto));
 			}
+
 			return resultado.toString().equals(digitoVerificadorCPF);
 		} else if (CNPJ_CPF.length() == 14) { // CNPJ
+
 			int acumulador = 0;
 			int digito = 0;
 			StringBuffer CNPJ = new StringBuffer();
 			char[] CNPJCharArray = CNPJ_CPF.toCharArray();
+
 			CNPJ.append(CNPJ_CPF.substring(0, 12));
+
 			for (int i = 0; i < 4; i++) {
 				if (((CNPJCharArray[i] - 48) >= 0)
 						&& ((CNPJCharArray[i] - 48) <= 9)) {
 					acumulador += (CNPJCharArray[i] - 48) * (6 - (i + 1));
 				}
 			}
+
 			for (int i = 0; i < 8; i++) {
 				if (((CNPJCharArray[i + 4] - 48) >= 0)
 						&& ((CNPJCharArray[i + 4] - 48) <= 9)) {
 					acumulador += (CNPJCharArray[i + 4] - 48) * (10 - (i + 1));
 				}
 			}
+
 			digito = 11 - (acumulador % 11);
+
 			CNPJ.append((digito == 10 || digito == 11) ? "0" : digito);
+
 			acumulador = 0;
+
 			for (int i = 0; i < 5; i++) {
 				if (((CNPJCharArray[i] - 48) >= 0)
 						&& ((CNPJCharArray[i] - 48) <= 9)) {
 					acumulador += (CNPJCharArray[i] - 48) * (7 - (i + 1));
 				}
 			}
+
 			for (int i = 0; i < 8; i++) {
 				if (((CNPJCharArray[i + 5] - 48) >= 0)
 						&& ((CNPJCharArray[i + 5] - 48) <= 9)) {
 					acumulador += (CNPJCharArray[i + 5] - 48) * (10 - (i + 1));
 				}
 			}
+
 			digito = 11 - (acumulador % 11);
+
 			CNPJ.append((digito == 10 || digito == 11) ? "0" : digito);
+
 			return CNPJ.toString().equals(CNPJ_CPF);
 		}
+
 		return false;
 	}
 
@@ -164,9 +189,9 @@ public abstract class Validator {
 		}
 		return formatar(cnpj, "##.###.###/####-##");
 	}
-	
+
 	public static String formatarData(String data) {
-		if (data != null &&  !("00000000").equalsIgnoreCase(data)) {
+		if (data != null && !("00000000").equalsIgnoreCase(data)) {
 			while (data.length() < 8) {
 				data = "0" + data;
 			}
@@ -191,4 +216,59 @@ public abstract class Validator {
 		return dataRetorno;
 	}
 
+	 public static boolean validaCPF(String cpf) { 
+		 cpf = cpf.replace(".", "").replace("-", "");
+	      boolean ret = false;  
+	      String base = "000000000";  
+	      String digitos = "00";  
+	      if (cpf.length() <= 11) {  
+	         if (cpf.length() < 11) {  
+	            cpf = base.substring(0, 11 - cpf.length()) + cpf;  
+	            base = cpf.substring(0, 9);  
+	         }  
+	         base = cpf.substring(0, 9);  
+	         digitos = cpf.substring(9, 11);  
+	         int soma = 0, mult = 11;  
+	         int[] var = new int[11];  
+	         // Recebe os números e realiza a multiplicação e soma.  
+	         for (int i = 0; i < 9; i++) {  
+	            var[i] = Integer.parseInt("" + cpf.charAt(i));  
+	            if (i < 9)  
+	               soma += (var[i] * --mult);  
+	         }  
+	         // Cria o primeiro dígito verificador.  
+	         int resto = soma % 11;  
+	         if (resto < 2) {  
+	            var[9] = 0;  
+	         } else {  
+	            var[9] = 11 - resto;  
+	         }  
+	         // Reinicia os valores.  
+	         soma = 0;  
+	         mult = 11;  
+	         // Realiza a multiplicação e soma do segundo dígito.  
+	         for (int i = 0; i < 10; i++)  
+	            soma += var[i] * mult--;  
+	         // Cria o segundo dígito verificador.  
+	         resto = soma % 11;  
+	         if (resto < 2) {  
+	            var[10] = 0;  
+	         } else {  
+	            var[10] = 11 - resto;  
+	         }  
+	         if ((digitos.substring(0, 1).equalsIgnoreCase(new Integer(var[9])  
+	               .toString()))  
+	               && (digitos.substring(1, 2).equalsIgnoreCase(new Integer(  
+	                     var[10]).toString()))) {  
+	            ret = true;  
+	         }  
+	      }  
+	  
+	      if (ret) {  
+	         System.out.println("O CPF  [" + cpf + "]    é   válido.");  
+	      } else {  
+	         System.out.println("O CPF  [" + cpf + "]    é inválido.");  
+	      }  
+	      return ret;  
+	   }  
 }
