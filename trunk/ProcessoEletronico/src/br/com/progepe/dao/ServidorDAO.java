aa7317ae3ca3ae22102fa4d288e0bacd26aac5ce
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-15 14:56:14 +0000

    #jefferson

--- trunk/ProcessoEletronico/src/br/com/progepe/dao/ServidorDAO.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/dao/ServidorDAO.java
@@ -8,43 +8,42 @@ import org.hibernate.criterion.Restrictions;
 
 import br.com.progepe.entity.Servidor;
 
-public class ServidorDAO extends DAO{
-
+public class ServidorDAO extends DAO {
 
 	@SuppressWarnings("unchecked")
 	public List<Servidor> listByFilter(Servidor servidor) {
 		HibernateUtility.getSession().clear();
 		HibernateUtility.beginTransaction();
-		
-		Criteria c = HibernateUtility.getSession().createCriteria(Servidor.class);
-		if (servidor.getSiape() != null && servidor.getSiape() != 0 ){
-		c.add(Restrictions.like("siape", servidor.getSiape()));
+
+		Criteria c = HibernateUtility.getSession().createCriteria(
+				Servidor.class);
+		if (servidor.getSiape() != null && servidor.getSiape() != 0) {
+			c.add(Restrictions.like("siape", servidor.getSiape()));
 		}
-		
-		if(servidor.getLotacao().getCodigo() != 0){
+
+		if (servidor.getLotacao().getCodigo() != 0) {
 			c.add(Restrictions.like("lotacao", servidor.getLotacao()));
 		}
-		
-		if (servidor.getNome() != null && servidor.getNome() != "" ){
-			c.add(Restrictions.like("nome", servidor.getNome().toUpperCase(), MatchMode.ANYWHERE));			
+
+		if (servidor.getNome() != null && servidor.getNome() != "") {
+			c.add(Restrictions.like("nome", servidor.getNome().toUpperCase(),
+					MatchMode.ANYWHERE));
 		}
-		
-		if (servidor.getCargo().getCodigo() != 0){
+
+		if (servidor.getCargo().getCodigo() != 0) {
 			c.add(Restrictions.like("cargo", servidor.getCargo()));
 		}
-		
+
 		return c.list();
 	}
-	
-	public Servidor refresBySiape(Servidor servidor) {
+
+	public Servidor refreshBySiape(Servidor servidor) {
 		HibernateUtility.getSession().clear();
 		HibernateUtility.beginTransaction();
-		
 		Criteria c = HibernateUtility.getSession().createCriteria(Servidor.class);
 		if (servidor.getSiape() != null && servidor.getSiape() != 0 ){
-		c.add(Restrictions.like("siape", servidor.getSiape()));
+			c.add(Restrictions.like("siape", servidor.getSiape()));
 		}
-		
 		return (Servidor) c.uniqueResult();
 	}
 	
