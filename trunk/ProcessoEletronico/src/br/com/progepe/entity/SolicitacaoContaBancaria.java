367076bc28a33068dad69f5b5f4e355d2f2dfc5c
nicole.u.dias <nicole.u.dias@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-18 10:56:44 +0000

    Criação da classe: SolicitacaoContaBancaria(.java)

---
+++ trunk/ProcessoEletronico/src/br/com/progepe/entity/SolicitacaoContaBancaria.java
@@ -0,0 +1,46 @@
+package br.com.progepe.entity;
+
+public class SolicitacaoContaBancaria extends Solicitacao {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String numeroConta;
+	private String agencia;
+	private Banco banco;
+	private Boolean indPoupanca = false;
+
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public Banco getBanco() {
+		return banco;
+	}
+
+	public void setBanco(Banco banco) {
+		this.banco = banco;
+	}
+
+	public Boolean getIndPoupanca() {
+		return indPoupanca;
+	}
+
+	public void setIndPoupanca(Boolean indPoupanca) {
+		this.indPoupanca = indPoupanca;
+	}
+	
+}
