fe93d3b503d23ec853ed9528d018638445e62d28
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-19 13:24:20 +0000

    #jefferson

---
+++ trunk/ProcessoEletronico/src/br/com/progepe/constantes/Constantes.java
@@ -0,0 +1,10 @@
+package br.com.progepe.constantes;
+
+public class Constantes {
+	public static final Long CAIXA_ECONOMICA_FEDERAL = 104L;
+	public static final Long STATUS_SOLICITACAO_AGUARDANDO = 1L;
+	public static final Long STATUS_SOLICITACAO_EM_APROVACAO = 2L;
+	public static final Long STATUS_SOLICITACAO_DEFERIDO = 3L;
+	public static final Long STATUS_SOLICITACAO_INDEFERIDO = 4L;
+	public static final Long TIPO_SOLICITACAO_ALTERAR_CONTA_BANCARIA = 1L;
+}
