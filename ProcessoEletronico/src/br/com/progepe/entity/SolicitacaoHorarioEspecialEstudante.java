2cd321b5d4b7c2e7b1c805d8f5bfb65c15a7b4bb
nicole.u.dias <nicole.u.dias@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-25 10:32:50 +0000

    *Marlon

---
+++ ProcessoEletronico/src/br/com/progepe/entity/SolicitacaoHorarioEspecialEstudante.java
@@ -0,0 +1,18 @@
+package br.com.progepe.entity;
+
+public class SolicitacaoHorarioEspecialEstudante extends Solicitacao {
+
+	private static final long serialVersionUID = 1L;
+		
+	private Boolean indAlimentacao = false;
+
+	public Boolean getIndAlimentacao() {
+		return indAlimentacao;
+	}
+
+	public void setIndAlimentacao(Boolean indAlimentacao) {
+		this.indAlimentacao = indAlimentacao;
+	}
+	
+	
+}
