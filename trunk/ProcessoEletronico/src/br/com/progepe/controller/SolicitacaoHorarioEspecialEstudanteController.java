8ca7799f7faf5e3a5c72888a8bb0673d7c93168e
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-27 18:04:27 +0000

    #jefferson

--- trunk/ProcessoEletronico/src/br/com/progepe/controller/SolicitacaoHorarioEspecialEstudanteController.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/controller/SolicitacaoHorarioEspecialEstudanteController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Serializable;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.faces.context.FacesContext;
@@ -20,21 +21,33 @@ import br.com.progepe.entity.SolicitacaoHorarioEspecialEstudante;
 import br.com.progepe.entity.StatusSolicitacao;
 import br.com.progepe.entity.TipoSolicitacao;
 
-public class SolicitacaoHorarioEspecialEstudanteController implements Serializable {
+public class SolicitacaoHorarioEspecialEstudanteController implements
+		Serializable {
 	private static final long serialVersionUID = -333995781063775201L;
 
 	private SolicitacaoHorarioEspecialEstudante solicitacaoHorarioEspecialEstudante;
+	private ArrayList<SolicitacaoHorarioEspecialEstudante> files = new ArrayList<SolicitacaoHorarioEspecialEstudante>();
 	DAO dao = new DAO();
 
 	public SolicitacaoHorarioEspecialEstudante getSolicitacaoHorarioEspecialEstudante() {
 		return solicitacaoHorarioEspecialEstudante;
 	}
 
-	public void setSolicitacaoHorarioEspecialEstudante(SolicitacaoHorarioEspecialEstudante solicitacaoHorarioEspecialEstudante) {
+	public void setSolicitacaoHorarioEspecialEstudante(
+			SolicitacaoHorarioEspecialEstudante solicitacaoHorarioEspecialEstudante) {
 		this.solicitacaoHorarioEspecialEstudante = solicitacaoHorarioEspecialEstudante;
 	}
 
-	public void abrirSolicitacaoHorarioEspecialEstudante() throws ParseException {
+	public ArrayList<SolicitacaoHorarioEspecialEstudante> getFiles() {
+		return files;
+	}
+
+	public void setFiles(ArrayList<SolicitacaoHorarioEspecialEstudante> files) {
+		this.files = files;
+	}
+
+	public void abrirSolicitacaoHorarioEspecialEstudante()
+			throws ParseException {
 		try {
 			solicitacaoHorarioEspecialEstudante = new SolicitacaoHorarioEspecialEstudante();
 			buscarServidorLogado();
@@ -54,30 +67,35 @@ public class SolicitacaoHorarioEspecialEstudanteController implements Serializab
 				siapeAutenticado.getSiape());
 		ServidorDAO servidorDAO = new ServidorDAO();
 		solicitacaoHorarioEspecialEstudante.setSolicitante(servidorDAO
-				.refreshBySiape(solicitacaoHorarioEspecialEstudante.getSolicitante()));
-	}
-	
-	public void listener(UploadEvent event) throws Exception {
-		UploadItem item = event.getUploadItem();
-		solicitacaoHorarioEspecialEstudante.setDeclaracaoMatricula(item.getData());
+				.refreshBySiape(solicitacaoHorarioEspecialEstudante
+						.getSolicitante()));
 	}
 
 	public void paint(OutputStream stream, Object object) throws IOException {
-			stream.write(this.solicitacaoHorarioEspecialEstudante.getDeclaracaoMatricula());
+		stream.write(getFiles().get((Integer) object).getDeclaracaoMatricula());
 	}
-	
+
+	public void listener(UploadEvent event) throws Exception {
+		UploadItem item = event.getUploadItem();
+		solicitacaoHorarioEspecialEstudante.setDeclaracaoMatricula(item.getData());
+		files.add(solicitacaoHorarioEspecialEstudante);
+	}
+
 	public void salvar() throws IOException, ParseException {
 		solicitacaoHorarioEspecialEstudante.setDataAbertura(new Date());
 		solicitacaoHorarioEspecialEstudante.setDataAtendimento(null);
-		solicitacaoHorarioEspecialEstudante.setTipoSolicitacao(new TipoSolicitacao());
+		solicitacaoHorarioEspecialEstudante
+				.setTipoSolicitacao(new TipoSolicitacao());
 		solicitacaoHorarioEspecialEstudante.getTipoSolicitacao().setCodigo(
 				Constantes.TIPO_SOLICITACAO_HORARIO_ESPECIAL_ESTUDANTE);
-		solicitacaoHorarioEspecialEstudante.setStatusSolicitacao(new StatusSolicitacao());
+		solicitacaoHorarioEspecialEstudante
+				.setStatusSolicitacao(new StatusSolicitacao());
 		solicitacaoHorarioEspecialEstudante.getStatusSolicitacao().setCodigo(
 				Constantes.STATUS_SOLICITACAO_ENCAMINHADO);
 		dao.saveOrUpdate(solicitacaoHorarioEspecialEstudante);
 		solicitacaoHorarioEspecialEstudante = new SolicitacaoHorarioEspecialEstudante();
+		files = new ArrayList<SolicitacaoHorarioEspecialEstudante>();
 		buscarServidorLogado();
 	}
-	
+
 }
