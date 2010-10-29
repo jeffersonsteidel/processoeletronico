86a5f16f04d6ed3d9b8b6f110927021c11a92ee6
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-10-29 13:34:45 +0000

    #jefferson

--- trunk/ProcessoEletronico/src/FitaEspelho.java
+++ trunk/ProcessoEletronico/src/FitaEspelho.java
@@ -283,8 +283,8 @@ public class FitaEspelho {
 			html = html + "<th>AGENCIA</th>";
 			html = html + "<th>CONTA CORRENTE</th>";
 			html = html + "<th>GRUPO/CARGO</th>";
-			html = html + "<th>CLASSE</th>";
 			html = html + "<th>PADRÃO</th>";
+			html = html + "<th>CLASSE</th>";
 			html = html + "<th>LOTAÇÃO</th>";
 			html = html + "<th>DATA ADMISSÃO SERV. PUB.</th>";
 			html = html + "<th>DATA ADMISSÃO ORGÃO.</th>";
