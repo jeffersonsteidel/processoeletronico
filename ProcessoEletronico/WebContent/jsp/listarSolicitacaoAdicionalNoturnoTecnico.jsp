9c8a39201a6103d101a7918d7ed8151ba918701c
nicole.u.dias <nicole.u.dias@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-12-07 14:01:23 +0000

    -tela para aprovação da solicitação de Adicional Noturno - Visível apenas para o Administrador. (Em função da internet, não foi possível testá-la). -Correção do Manual - 100%

---
+++ ProcessoEletronico/WebContent/jsp/listarSolicitacaoAdicionalNoturnoTecnico.jsp
@@ -0,0 +1,104 @@
+<%@ taglib prefix="f" uri="http://java.sun.com/jsf/core"%>
+<%@ taglib prefix="h" uri="http://java.sun.com/jsf/html"%>
+<%@ taglib uri="http://richfaces.org/a4j" prefix="a4j"%>
+<%@ taglib uri="http://richfaces.org/rich" prefix="rich"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Instituto Federal do Paraná</title>
+</head>
+<body>
+<f:view>
+	<jsp:directive.include file="menus.jsp" />
+	<center><a4j:form id="form">
+		<rich:panel>
+			<rich:messages layout="list">
+				<f:facet name="errorMarker">
+					<h:graphicImage value="../images/error.gif" />
+				</f:facet>
+			</rich:messages>
+			<font size="2"><b>SOLICITAÇÕES DE ADICIONAL NOTURNO -
+			TÉCNICO</b></font>
+			<h:panelGrid columns="2">
+				<h:outputText
+					value="#{solicitacaoAdicionalNoturnoController.solicitacaoAdicionalNoturno.servidor.siape} - #{solicitacaoAdicionalNoturnoController.solicitacaoAdicionalNoturno.servidor.nome}">
+				</h:outputText>
+			</h:panelGrid>
+
+
+			<h:panelGrid columns="3" id="campus">
+				<h:outputText value="Campus: " />
+				<h:selectOneMenu
+					value="#{solicitacaoAdicionalNoturnoController.solicitacaoAdicionalNoturno.lotacao.codigo}">
+					<f:selectItem itemLabel="SELECIONE" itemValue="" />
+					<f:selectItems
+						value="#{solicitacaoAdicionalNoturnoController.lotacoes}" />
+				</h:selectOneMenu>
+
+				<a4j:commandButton value="Pesquisar" action="#"
+					reRender="listaSolicitacoesAdicionalTecnicos" />
+
+			</h:panelGrid>
+
+			<h:panelGrid columns="1">
+
+				<rich:dataTable id="listaSolicitacoesAdicionalTecnicos"
+					value="#{solicitacaoAdicionaltNoturnoController.listaSolicitacoesAdicionalTecnicos}"
+					var="list" width="1160px" columnClasses="center" rows="15"
+					reRender="ds">
+
+					<rich:column width="435px" sortBy="#{list.nome}">
+						<f:facet name="header">
+							<h:outputText value="Nome" />
+						</f:facet>
+						<h:outputText value="#{list.servidor.nome}" />
+					</rich:column>
+
+					<rich:column width="435px">
+						<f:facet name="header">
+							<h:outputText value="Data" />
+						</f:facet>
+						<h:outputText value="#{list.data}">
+							<f:convertDateTime pattern="dd/MM/yyyy" />
+						</h:outputText>
+					</rich:column>
+
+					<rich:column width="435px">
+						<f:facet name="header">
+							<h:outputText value="Dia da Semana" />
+						</f:facet>
+						<h:outputText value="#{list.diaSemana}">
+						</h:outputText>
+					</rich:column>
+
+					<rich:column width="435px">
+						<f:facet name="header">
+							<h:outputText value="Horário" />
+						</f:facet>
+						<h:outputText value="#{list.horaInicial} - #{list.horaFinal}" />
+					</rich:column>
+
+					<rich:column width="435px">
+						<f:facet name="header">
+							<h:outputText value="Motivo" />
+						</f:facet>
+						<h:outputText value="#{list.motivo}" />
+					</rich:column>
+
+					<rich:column width="435px">
+						<f:facet name="header">
+							<h:outputText value="Aprovação" />
+						</f:facet>
+						<h:outputText value="#" />
+					</rich:column>
+					<f:facet name="footer">
+						<rich:datascroller id="ds"></rich:datascroller>
+					</f:facet>
+				</rich:dataTable>
+			</h:panelGrid>
+		</rich:panel>
+	</a4j:form></center>
+</f:view>
+</body>
+</html>
---
+++ ProcessoEletronico/src/br/com/progepe/dao/AdicionalNoturnoDAO.java
@@ -0,0 +1,150 @@
+package br.com.progepe.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.criterion.Restrictions;
+
+import br.com.progepe.entity.AdicionalNoturno;
+import br.com.progepe.entity.Lotacao;
+import br.com.progepe.entity.Solicitacao;
+import br.com.progepe.entity.SolicitacaoAdicionalNoturno;
+import br.com.progepe.entity.SolicitacaoAfastamentoConjuge;
+import br.com.progepe.entity.SolicitacaoAlimentacao;
+import br.com.progepe.entity.SolicitacaoCasamento;
+import br.com.progepe.entity.SolicitacaoContaBancaria;
+import br.com.progepe.entity.SolicitacaoHorarioEspecialEstudante;
+import br.com.progepe.entity.SolicitacaoLicencaPaternidade;
+import br.com.progepe.entity.SolicitacaoObito;
+
+public class AdicionalNoturnoDAO extends DAO {
+
+	private static AdicionalNoturnoDAO instance;
+	private AdicionalNoturnoDAO(){}
+	
+	
+	public static AdicionalNoturnoDAO getInstance(){
+		if(instance == null){
+			instance = new AdicionalNoturnoDAO();
+		}
+		return instance;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<AdicionalNoturno> listByFilter(Lotacao lotacao) {
+		HibernateUtility.getSession().clear();
+
+		Criteria c = HibernateUtility.getSession().createCriteria(
+				SolicitacaoAdicionalNoturno.class);
+//		if (solicitacao.getServidor().getNome() != null
+//				&& solicitacao.getServidor().getNome() != "") {
+//			c.add(Restrictions.like("servidor", solicitacao.getServidor()));
+//		}
+//		if (dataSolicitacaoInicial != null && dataSolicitacaoFinal != null) {
+//			c.add(Restrictions.between("dataAbertura", dataSolicitacaoInicial,
+//					dataSolicitacaoFinal));
+//		}
+//
+//		if (solicitacao.getStatusSolicitacao().getCodigo() != null
+//				&& solicitacao.getStatusSolicitacao().getCodigo() != 0) {
+//			c.add(Restrictions.like("statusSolicitacao",
+//					solicitacao.getStatusSolicitacao()));
+//		}
+//
+//		if (solicitacao.getTipoSolicitacao().getCodigo() != null
+//				&& solicitacao.getTipoSolicitacao().getCodigo() != 0) {
+//			c.add(Restrictions.like("tipoSolicitacao",
+//					solicitacao.getTipoSolicitacao()));
+//		}
+		HibernateUtility.commitTransaction();
+		return c.list();
+	}
+
+	public SolicitacaoContaBancaria carregarSoliciacaoContaBancaria(Long codigo) {
+		HibernateUtility.getSession().clear();
+		Query query = HibernateUtility.getSession().createQuery(
+				"from SolicitacaoContaBancaria  s where s.codigo= :codigo");
+		query.setParameter("codigo", codigo);
+		HibernateUtility.commitTransaction();
+		return (SolicitacaoContaBancaria) query.uniqueResult();
+	}
+	
+	public SolicitacaoCasamento carregarSolicitacaoCasamento(Long codigo) {
+		HibernateUtility.getSession().clear();
+		Query query = HibernateUtility.getSession().createQuery(
+				"from SolicitacaoCasamento  s where s.codigo= :codigo");
+		query.setParameter("codigo", codigo);
+		HibernateUtility.commitTransaction();
+		return (SolicitacaoCasamento) query.uniqueResult();
+	}
+	
+	public SolicitacaoAlimentacao carregarSolicitacaoAlimentacao(Long codigo) {
+		HibernateUtility.getSession().clear();
+		Query query = HibernateUtility.getSession().createQuery(
+				"from SolicitacaoAlimentacao  s where s.codigo= :codigo");
+		query.setParameter("codigo", codigo);
+		HibernateUtility.commitTransaction();
+		return (SolicitacaoAlimentacao) query.uniqueResult();
+	}
+
+	public SolicitacaoLicencaPaternidade carregarSolicitacaoLicencaPaternidade(
+			Long codigo) {
+		HibernateUtility.getSession().clear();
+		Query query = HibernateUtility.getSession().createQuery(
+				"from SolicitacaoLicencaPaternidade s where s.codigo= :codigo");
+		query.setParameter("codigo", codigo);
+		HibernateUtility.commitTransaction();
+		return (SolicitacaoLicencaPaternidade) query.uniqueResult();
+	}
+	
+	public SolicitacaoAfastamentoConjuge carregarSolicitacaoAfastamentoConjuge(
+			Long codigo) {
+		HibernateUtility.getSession().clear();
+		Query query = HibernateUtility.getSession().createQuery(
+				"from SolicitacaoAfastamentoConjuge s where s.codigo= :codigo");
+		query.setParameter("codigo", codigo);
+		HibernateUtility.commitTransaction();
+		return (SolicitacaoAfastamentoConjuge) query.uniqueResult();
+	}
+	
+	
+	public SolicitacaoHorarioEspecialEstudante carregarSolicitacaoHorarioEspecialEstudante(
+			Long codigo) {
+		HibernateUtility.getSession().clear();
+		Query query = HibernateUtility.getSession().createQuery(
+				"from SolicitacaoHorarioEspecialEstudante s where s.codigo= :codigo");
+		query.setParameter("codigo", codigo);
+		HibernateUtility.commitTransaction();
+		return (SolicitacaoHorarioEspecialEstudante) query.uniqueResult();
+	}
+	
+	public SolicitacaoObito carregarSolicitacaoObito(
+			Long codigo) {
+		HibernateUtility.getSession().clear();
+		Query query = HibernateUtility.getSession().createQuery(
+				"from SolicitacaoObito s where s.codigo= :codigo");
+		query.setParameter("codigo", codigo);
+		HibernateUtility.commitTransaction();
+		return (SolicitacaoObito) query.uniqueResult();
+	}
+	
+	public void updateSolicitacao(Object objeto) {
+		try {
+			HibernateUtility.getSession().clear();
+			HibernateUtility.getSession().saveOrUpdate(objeto);
+			HibernateUtility.commitTransaction();
+		} catch (Exception e) {
+			HibernateUtility.rollbackTransaction();
+			FacesMessage message = new FacesMessage(
+					FacesMessage.SEVERITY_ERROR,
+					"Erro ao comunicar com o servidor!",
+					"Erro ao comunicar com o servidor!");
+			FacesContext.getCurrentInstance().addMessage("", message);
+		}
+	}
+}
