547d176359ceeea40df0f54afdafbbf606e5ea43
nicole.u.dias <nicole.u.dias@139865b2-675c-e321-0b12-2b09a3ca379a>
2010-12-21 13:11:02 +0000

    Controller e JSP para NOVA FUNÇÃO

---
+++ trunk/ProcessoEletronico/src/br/com/progepe/controller/FuncaoController.java
@@ -0,0 +1,80 @@
+package br.com.progepe.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+
+import br.com.progepe.dao.DAO;
+import br.com.progepe.entity.Funcao;
+import br.com.progepe.entity.TipoFuncao;
+
+public class FuncaoController implements Serializable {
+
+	private static final long serialVersionUID = 8505987470598069907L;
+
+	private Funcao funcao;
+	private List<SelectItem> tipoFuncoes = new ArrayList<SelectItem>();
+	private List<SelectItem> funcoes = new ArrayList<SelectItem>();
+	private Boolean indAtual = false;
+
+	public Funcao getFuncao() {
+		return funcao;
+	}
+
+	public void setFuncao(Funcao funcao) {
+		this.funcao = funcao;
+	}
+
+	public List<SelectItem> getTipoFuncoes() {
+		return tipoFuncoes;
+	}
+
+	public void setTipoFuncoes(List<SelectItem> tipoFuncoes) {
+		this.tipoFuncoes = tipoFuncoes;
+	}
+
+	public List<SelectItem> getFuncoes() {
+		return funcoes;
+	}
+
+	public void setFuncoes(List<SelectItem> funcoes) {
+		this.funcoes = funcoes;
+	}
+
+	public Boolean getIndAtual() {
+		return indAtual;
+	}
+
+	public void setIndAtual(Boolean indAtual) {
+		this.indAtual = indAtual;
+	}
+
+	public void abrirNovaFuncao() throws Exception {
+		funcao = new Funcao();
+		funcao.setTipoFuncao(new TipoFuncao());
+		listarTipoFuncoes();
+		FacesContext.getCurrentInstance().getExternalContext()
+				.redirect("novaFuncao.jsp");
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<SelectItem> listarTipoFuncoes() {
+		tipoFuncoes = new ArrayList<SelectItem>();
+		List<TipoFuncao> tipoFuncaoList = new ArrayList<TipoFuncao>();
+		tipoFuncaoList = DAO.getInstance().list(TipoFuncao.class, "codigo");
+		for (TipoFuncao item : tipoFuncaoList) {
+			tipoFuncoes.add(new SelectItem(item.getCodigo(), item.getSigla()));
+		}
+		return tipoFuncoes;
+	}
+
+	public void salvarNovaFuncao() {
+		DAO.getInstance().saveOrUpdate(funcao);
+		funcao = new Funcao();
+		funcao.setTipoFuncao(new TipoFuncao());
+	}
+
+}
