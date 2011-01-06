567c768d4d7fce4b90843e123b63ebc812c7bcd1
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2011-01-06 15:11:10 +0000

    SOLICITAÇÃO ALTERAÇÃO DE ENDEREÇO

--- ProcessoEletronico/src/br/com/progepe/controller/SolicitacaoAlteracaoEnderecoController.java
+++ ProcessoEletronico/src/br/com/progepe/controller/SolicitacaoAlteracaoEnderecoController.java
@@ -3,14 +3,20 @@ package br.com.progepe.controller;
 import java.io.IOException;
 import java.io.Serializable;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 
 import br.com.progepe.constantes.Constantes;
+import br.com.progepe.dao.CidadeDAO;
 import br.com.progepe.dao.DAO;
 import br.com.progepe.dao.ServidorDAO;
 import br.com.progepe.entity.Autenticacao;
+import br.com.progepe.entity.Cidade;
+import br.com.progepe.entity.Estado;
 import br.com.progepe.entity.RegimeTrabalho;
 import br.com.progepe.entity.Servidor;
 import br.com.progepe.entity.SolicitacaoAlteracaoEndereco;
@@ -21,6 +27,9 @@ public class SolicitacaoAlteracaoEnderecoController implements Serializable {
 	private static final long serialVersionUID = -333995781063775201L;
 
 	private SolicitacaoAlteracaoEndereco solicitacaoAlteracaoEndereco;
+	
+	private List<SelectItem> cidades = new ArrayList<SelectItem>();
+	private List<SelectItem> estados = new ArrayList<SelectItem>();
 
 	public SolicitacaoAlteracaoEndereco getSolicitacaoAlteracaoEndereco() {
 		return solicitacaoAlteracaoEndereco;
@@ -30,10 +39,28 @@ public class SolicitacaoAlteracaoEnderecoController implements Serializable {
 			SolicitacaoAlteracaoEndereco solicitacaoAlteracaoEndereco) {
 		this.solicitacaoAlteracaoEndereco = solicitacaoAlteracaoEndereco;
 	}
+	
+	public List<SelectItem> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<SelectItem> cidades) {
+		this.cidades = cidades;
+	}
+
+	public List<SelectItem> getEstados() {
+		return estados;
+	}
+
+	public void setEstados(List<SelectItem> estados) {
+		this.estados = estados;
+	}
 
 	public void abrirSolicitacaoAlteracaoEndereco() throws ParseException {
 		try {
 			solicitacaoAlteracaoEndereco = new SolicitacaoAlteracaoEndereco();
+			solicitacaoAlteracaoEndereco.setNovaCidade(new Cidade());
+			solicitacaoAlteracaoEndereco.getNovaCidade().setEstado(new Estado());
 			buscarServidorLogado();
 			FacesContext.getCurrentInstance().getExternalContext()
 					.redirect("solicitacaoAlteracaoEndereco.jsp");
@@ -66,6 +93,31 @@ public class SolicitacaoAlteracaoEnderecoController implements Serializable {
 				Constantes.STATUS_SOLICITACAO_ENCAMINHADO);
 		DAO.getInstance().saveOrUpdate(solicitacaoAlteracaoEndereco);
 		solicitacaoAlteracaoEndereco = new SolicitacaoAlteracaoEndereco();
+		solicitacaoAlteracaoEndereco.setNovaCidade(new Cidade());
+		solicitacaoAlteracaoEndereco.getNovaCidade().setEstado(new Estado());
 		buscarServidorLogado();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<SelectItem> listarEstados() {
+		estados = new ArrayList<SelectItem>();
+		List<Estado> estadoList = new ArrayList<Estado>();
+		estadoList = DAO.getInstance().list(Estado.class, "descricao");
+		for (Estado estado : estadoList) {
+			estados.add(new SelectItem(estado.getCodigo(), estado
+					.getDescricao()));
+		}
+		return estados;
+	}
+
+	public List<SelectItem> listarCidades() {
+		cidades = new ArrayList<SelectItem>();
+		List<Cidade> cidadeList = new ArrayList<Cidade>();
+		cidadeList = CidadeDAO.getInstance().listByEstado(solicitacaoAlteracaoEndereco.getNovaCidade().getEstado());
+		for (Cidade cidade : cidadeList) {
+			cidades.add(new SelectItem(cidade.getCodigo(), cidade
+					.getDescricao()));
+		}
+		return cidades;
 	}
 }
