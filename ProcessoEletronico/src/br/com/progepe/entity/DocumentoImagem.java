6a6d7b29e037f2dccee50bf207151f28eaf170c2
marlontaborda.ifpr <marlontaborda.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2011-01-11 17:27:45 +0000

    ANEXAR DOCUMENTOS

---
+++ ProcessoEletronico/src/br/com/progepe/entity/DocumentoImagem.java
@@ -0,0 +1,102 @@
+package br.com.progepe.entity;
+
+import java.io.Serializable;
+
+public class DocumentoImagem implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1254981489993800216L;
+	private Long codigo;
+	private TipoDocumento tipoDocumento;
+	private Servidor servidor;
+	private Conjuge conjuge;
+	private Dependente dependente;
+	private byte[] imagem1;
+	private byte[] imagem2;
+	private byte[] imagem3;
+	private Boolean indValidado;
+	private String motivo;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public TipoDocumento getTipoDocumento() {
+		return tipoDocumento;
+	}
+
+	public void setTipoDocumento(TipoDocumento tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+
+	public Servidor getServidor() {
+		return servidor;
+	}
+
+	public void setServidor(Servidor servidor) {
+		this.servidor = servidor;
+	}
+
+	public Conjuge getConjuge() {
+		return conjuge;
+	}
+
+	public void setConjuge(Conjuge conjuge) {
+		this.conjuge = conjuge;
+	}
+
+	public Dependente getDependente() {
+		return dependente;
+	}
+
+	public void setDependente(Dependente dependente) {
+		this.dependente = dependente;
+	}
+
+	public byte[] getImagem1() {
+		return imagem1;
+	}
+
+	public void setImagem1(byte[] imagem1) {
+		this.imagem1 = imagem1;
+	}
+
+	public byte[] getImagem2() {
+		return imagem2;
+	}
+
+	public void setImagem2(byte[] imagem2) {
+		this.imagem2 = imagem2;
+	}
+
+	public byte[] getImagem3() {
+		return imagem3;
+	}
+
+	public void setImagem3(byte[] imagem3) {
+		this.imagem3 = imagem3;
+	}
+
+	public Boolean getIndValidado() {
+		return indValidado;
+	}
+
+	public void setIndValidado(Boolean indValidado) {
+		this.indValidado = indValidado;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+
+}
