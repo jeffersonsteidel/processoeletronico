106556f3e072cfc4453a67a8398433005d9ef294
marlontaborda.ifpr <marlontaborda.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2011-01-12 16:37:32 +0000

    ANEXAR DOCUMENTOS

--- trunk/ProcessoEletronico/src/br/com/progepe/dao/DocumentoImagemDAO.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/dao/DocumentoImagemDAO.java
@@ -7,7 +7,9 @@ import org.hibernate.criterion.Restrictions;
 
 import br.com.progepe.entity.Conjuge;
 import br.com.progepe.entity.Dependente;
+import br.com.progepe.entity.DocumentoImagem;
 import br.com.progepe.entity.Servidor;
+import br.com.progepe.entity.Solicitacao;
 
 public class DocumentoImagemDAO extends DAO {
 
@@ -44,6 +46,33 @@ public class DocumentoImagemDAO extends DAO {
 				Dependente.class);
 		if (servidor != null) {
 			c.add(Restrictions.like("servidor", servidor));
+		}
+		HibernateUtility.commitTransaction();
+		return c.list();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<DocumentoImagem> listByFilter(DocumentoImagem documentoImagem,
+			Integer titularDocumento) {
+		HibernateUtility.beginTransaction();
+		HibernateUtility.getSession().clear();
+
+		Criteria c = HibernateUtility.getSession().createCriteria(
+				Solicitacao.class);
+		if (documentoImagem.getServidor() != null
+				&& documentoImagem.getServidor().getSiape() != 0) {
+			c.add(Restrictions.like("servidor", documentoImagem.getServidor()));
+		}
+		if (titularDocumento.equals(2)) {
+			c.add(Restrictions.isNotNull("conjuge"));
+		}
+		if (titularDocumento.equals(3)) {
+			c.add(Restrictions.isNotNull("dependente"));
+		}
+		if (documentoImagem.getTipoDocumento() != null
+				&& documentoImagem.getTipoDocumento().getCodigo() != 0) {
+			c.add(Restrictions.like("tipoDocumento",
+					documentoImagem.getTipoDocumento()));
 		}
 		HibernateUtility.commitTransaction();
 		return c.list();
