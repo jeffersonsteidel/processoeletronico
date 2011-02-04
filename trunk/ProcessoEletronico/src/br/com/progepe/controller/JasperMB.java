96bb2ea59cae502474ae4614a50b7ce9e8b8a7b1
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2011-02-04 18:47:48 +0000

    ####### MANUTENÇÃO ########

--- trunk/ProcessoEletronico/src/br/com/progepe/controller/JasperMB.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/controller/JasperMB.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
@@ -14,7 +15,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRResultSetDataSource;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -59,73 +59,47 @@ public class JasperMB {
 		return session.getServletContext().getContextPath();
 	}
 
-	private void preenchePdf(JasperPrint print) throws JRException, Exception {
-        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext(); // Context    
-        HttpServletResponse response = (HttpServletResponse) context.getResponse();    
-        byte[] arquivo = new byte[0];  
-		  try{
-	        String caminho = getDiretorioReal("/WEB-INF/jasper/ralatorio1.jasper");  
-	        JasperReport jasper = (JasperReport) JRLoader.loadObject(caminho);
-	        print = JasperFillManager.fillReport(jasper, null, conexao);  
-	        arquivo = JasperExportManager.exportReportToPdf(print);  
-	  
-	        response.setContentType("application/pdf");  
-	        response.setContentLength(arquivo.length);  
-	  
-	        OutputStream saida = response.getOutputStream();  
-	  
-	        saida.write(arquivo, 0, arquivo.length);  
-	        saida.flush();  
-	        saida.close();  
+	private void preenchePdf(JasperPrint print, String nomeDoJasper) throws JRException, Exception {
+		ExternalContext context = FacesContext.getCurrentInstance()
+				.getExternalContext(); // Context
+		HttpServletResponse response = (HttpServletResponse) context
+				.getResponse();
+		byte[] arquivo = new byte[0];
+		try {
+			String caminho = getDiretorioReal(nomeDoJasper);
+			JasperReport jasper = (JasperReport) JRLoader.loadObject(caminho);
+			print = JasperFillManager.fillReport(jasper, null, conexao);
+			arquivo = JasperExportManager.exportReportToPdf(print);
+
+			response.setContentType("application/pdf");
+			response.setContentLength(arquivo.length);
+
+			OutputStream saida = response.getOutputStream();
+
+			saida.write(arquivo, 0, arquivo.length);
+			saida.flush();
+			saida.close();
 			FacesContext.getCurrentInstance().responseComplete();
 		} catch (IOException ex) {
 			System.out.println("Error in downloadFile: " + ex.getMessage());
 			ex.printStackTrace();
-		}       
-       
-    }    
-
-
-	public String geraRelatorioPassandoConexao() {
-		saida = null;
-		String jasper = getDiretorioReal("/WEB-INF/jasper/ralatorio1.jasper");
-		Connection conexao = null;
-		try {
-			// Abro a conexão com o banco que será passada para o JasperReports
-			conexao = getConexao();
-			// Mando o jasper gerar o relatório
-			JasperPrint print = JasperFillManager.fillReport(jasper, null,
-					conexao);
-			// Gero o PDF
-			preenchePdf(print); // VEJA O MÉTODO NO CAPÍTULO 3 DO TUTORIAL
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				// Sempre mando fechar a conexão, mesmo que tenha dado erro
-				if (conexao != null)
-					conexao.close();
-			} catch (SQLException e) {
-			}
 		}
-		return "exibeRelatorio";
+
 	}
 
-	public void geraRelatorioPassandoResultSet() throws JRException {
+	@SuppressWarnings("rawtypes")
+	public void geraRelatorioPassandoResultSet(HashMap parametros,String nomeDoJasper)
+			throws JRException {
 		saida = null;
-		String jasper = getDiretorioReal("/WEB-INF/jasper/ralatorio1.jasper");
+		String jasper = getDiretorioReal(nomeDoJasper);
 		Connection conexao = null;
 		try {
 			// Abro a conexão com o banco
 			conexao = getConexao();
-			// Gero o ResultSet que será enviado a partir da conexão aberta
-			JRResultSetDataSource jrsds = new JRResultSetDataSource(
-					getResultSet());
-			// Mando o jasper gerar o relatório
-			JasperPrint print = JasperFillManager.fillReport(jasper, null,
-					jrsds);
+			JasperPrint print = JasperFillManager.fillReport(jasper,
+					parametros, conexao);
 			// Gero o PDF
-			preenchePdf(print);
+			preenchePdf(print, nomeDoJasper);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -138,18 +112,10 @@ public class JasperMB {
 		}
 	}
 
-	public ResultSet getResultSet() throws SQLException, ClassNotFoundException {
+	public ResultSet getResultSet(String sql) throws SQLException,
+			ClassNotFoundException {
 		Statement stmt = conexao.createStatement();
-		ResultSet rs = stmt
-				.executeQuery("SELECT servidor.`serv_nome` AS servidor_serv_nome FROM `servidor` servidor");
+		ResultSet rs = stmt.executeQuery(sql);
 		return rs;
 	}
-
-	// public static void main(String[] args) throws ClassNotFoundException,
-	// SQLException {
-	// JasperMB jasperMB = new JasperMB();
-	// jasperMB.criaConexao();
-	// jasperMB.getResultSet();
-	// jasperMB.geraRelatorioPassandoResultSet();
-	// }
 }
