d890a90094630a19914d127a96a126af0fffc8ca
evandro.stresser <evandro.stresser@139865b2-675c-e321-0b12-2b09a3ca379a>
2011-02-21 13:11:06 +0000

    RELATÓRIOS

--- trunk/ProcessoEletronico/src/br/com/progepe/controller/RelatorioController.java
+++ trunk/ProcessoEletronico/src/br/com/progepe/controller/RelatorioController.java
@@ -3,19 +3,30 @@ package br.com.progepe.controller;
 import java.io.IOException;
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 
 import net.sf.jasperreports.engine.JRException;
+import br.com.progepe.dao.DAO;
 import br.com.progepe.dao.ServidorDAO;
+import br.com.progepe.entity.Cargo;
+import br.com.progepe.entity.Lotacao;
 import br.com.progepe.entity.Servidor;
 
 public class RelatorioController implements Serializable {
 	private static final long serialVersionUID = -333995781063775201L;
 	private Servidor servidor;
-	
+	private List<SelectItem> cargos = new ArrayList<SelectItem>();
+	private Integer cargo;
+	private List<SelectItem> locaisExercicio = new ArrayList<SelectItem>();
+	private Integer lotacao;
+	private Integer situacao;
+
 	public Servidor getServidor() {
 		return servidor;
 	}
@@ -23,12 +34,87 @@ public class RelatorioController implements Serializable {
 	public void setServidor(Servidor servidor) {
 		this.servidor = servidor;
 	}
+
+	public List<SelectItem> getCargos() {
+		return cargos;
+	}
+
+	public void setCargos(List<SelectItem> cargos) {
+		this.cargos = cargos;
+	}
+	
+	public Integer getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(Integer cargo) {
+		this.cargo = cargo;
+	}
+
+	public Integer getLotacao() {
+		return lotacao;
+	}
+
+	public void setLotacao(Integer lotacao) {
+		this.lotacao = lotacao;
+	}
+
+	public List<SelectItem> getLocaisExercicio() {
+		return locaisExercicio;
+	}
+
+	public void setLocaisExercicio(List<SelectItem> locaisExercicio) {
+		this.locaisExercicio = locaisExercicio;
+	}
+
+	public Integer getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Integer situacao) {
+		this.situacao = situacao;
+	}
+
+	public void abrirRelatorioCargoLotacaoByFiltro() throws Exception {
+		try {
+			listarCargos();
+			listarLotacoes();
+			FacesContext.getCurrentInstance().getExternalContext()
+					.redirect("relatorioCargoLotacao.jsp");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<SelectItem> listarCargos() {
+		cargos = new ArrayList<SelectItem>();
+		List<Cargo> cargoList = new ArrayList<Cargo>();
+		cargoList = DAO.getInstance().list(Cargo.class, "descricao");
+		for (Cargo cargo : cargoList) {
+			cargos.add(new SelectItem(cargo.getCodigo(), cargo
+					.getDescricao()));
+		}
+		return cargos;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<SelectItem> listarLotacoes() {
+		locaisExercicio = new ArrayList<SelectItem>();
+		List<Lotacao> lotacaoList = new ArrayList<Lotacao>();
+		lotacaoList = DAO.getInstance().list(Lotacao.class, "descricao");
+		for (Lotacao lotacao : lotacaoList) {
+			locaisExercicio.add(new SelectItem(lotacao.getCodigo(), lotacao
+					.getDescricao()));
+		}
+		return locaisExercicio;
+	}
 	
 	public void abrirRelatorios() throws IOException {
 		FacesContext.getCurrentInstance().getExternalContext()
 				.redirect("relatorios.jsp");
 	}
-	
+
 	public String abrirRelatorioGeral() throws Exception {
 		try {
 			servidor = new Servidor();
@@ -39,74 +125,112 @@ public class RelatorioController implements Serializable {
 		}
 		return "";
 	}
-	
-	@SuppressWarnings({"rawtypes", "unchecked"})
-	public String abrirRelatorioPermissaoUsuario() throws ClassNotFoundException,
-			SQLException, JRException {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public String abrirRelatorioPermissaoUsuario()
+			throws ClassNotFoundException, SQLException, JRException {
 		JasperMB jasperMB = new JasperMB();
 		jasperMB.criaConexao();
 		HashMap parametros = new HashMap();
-		parametros.put("BANNER", jasperMB.getDiretorioReal("/images/banner_topo.gif"));
+		parametros.put("BANNER",
+				jasperMB.getDiretorioReal("/images/banner_topo.gif"));
 		String nomeDoJasper = "/WEB-INF/jasper/relatorioPermissoesUsuario.jasper";
-		jasperMB.geraRelatorioPassandoResultSet(parametros,nomeDoJasper);
+		jasperMB.geraRelatorioPassandoResultSet(parametros, nomeDoJasper);
 		return "";
 	}
-	
-	@SuppressWarnings({"rawtypes", "unchecked"})
-	public String gerarRelatorioServidorCargoLotacao() throws ClassNotFoundException,
-			SQLException, JRException {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public String gerarRelatorioServidorCargoLotacao()
+			throws ClassNotFoundException, SQLException, JRException {
 		JasperMB jasperMB = new JasperMB();
 		jasperMB.criaConexao();
 		HashMap parametros = new HashMap();
-		parametros.put("BANNER", jasperMB.getDiretorioReal("/images/banner_topo.gif"));
+		parametros.put("BANNER",
+				jasperMB.getDiretorioReal("/images/banner_topo.gif"));
 		String nomeDoJasper = "/WEB-INF/jasper/relatorioServidorCargoLotacao.jasper";
-		jasperMB.geraRelatorioPassandoResultSet(parametros,nomeDoJasper);
+		jasperMB.geraRelatorioPassandoResultSet(parametros, nomeDoJasper);
 		return "";
 	}
 	
-	@SuppressWarnings({"rawtypes", "unchecked"})
-	public String gerarRelatorioServidorContaBancaria() throws ClassNotFoundException,
-			SQLException, JRException {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public String gerarRelatorioServidorCargoLotacaoByFiltro()
+			throws ClassNotFoundException, SQLException, JRException {
 		JasperMB jasperMB = new JasperMB();
 		jasperMB.criaConexao();
 		HashMap parametros = new HashMap();
-		parametros.put("BANNER", jasperMB.getDiretorioReal("/images/banner_topo.gif"));
-		String nomeDoJasper = "/WEB-INF/jasper/relatorioServidorContaBancaria.jasper";
-		jasperMB.geraRelatorioPassandoResultSet(parametros,nomeDoJasper);
+		parametros.put("BANNER",
+				jasperMB.getDiretorioReal("/images/banner_topo.gif"));
+		String nomeDoJasper = "/WEB-INF/jasper/relatorioServidorCargoLotacao.jasper";
+		jasperMB.geraRelatorioPassandoResultSet(parametros, nomeDoJasper);
 		return "";
 	}
-	
-	@SuppressWarnings({"rawtypes", "unchecked"})
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public String gerarRelatorioServidorContaBancaria()
+			throws ClassNotFoundException, SQLException, JRException {
+		JasperMB jasperMB = new JasperMB();
+		jasperMB.criaConexao();
+		HashMap parametros = new HashMap();
+		parametros.put("BANNER",
+				jasperMB.getDiretorioReal("/images/banner_topo.gif"));
+		String nomeDoJasper = "/WEB-INF/jasper/relatorioServidorContaBancaria.jasper";
+		jasperMB.geraRelatorioPassandoResultSet(parametros, nomeDoJasper);
+		return "";
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public String gerarRelatorioServidorGeral() throws ClassNotFoundException,
 			SQLException, JRException {
-		servidor = (Servidor) ServidorDAO.getInstance().refreshBySiape(servidor);
-		if(servidor == null){
+		servidor = (Servidor) ServidorDAO.getInstance()
+				.refreshBySiape(servidor);
+		if (servidor == null) {
 			FacesMessage message = new FacesMessage(
 					FacesMessage.SEVERITY_ERROR, "Siape inválido!",
 					"Siape inválido!");
 			FacesContext.getCurrentInstance().addMessage("", message);
-		}else{
-		JasperMB jasperMB = new JasperMB();
-		jasperMB.criaConexao();
-		HashMap parametros = new HashMap();
-		parametros.put("BANNER", jasperMB.getDiretorioReal("/images/banner_topo.gif"));
-		parametros.put("SUB_PESSOAIS", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubPessoais.jasper"));
-		parametros.put("SUB_FUNCIONAL", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubDadosFuncionais.jasper"));
-		parametros.put("SUB_ENDERECO", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubContatoEndereco.jasper"));
-		parametros.put("SUB_DOCUMENTOS", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubDocumentos.jasper"));
-		parametros.put("SUB_CONTA_BANCARIA", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubContaBancaria.jasper"));
-		parametros.put("SUB_TITULACOES", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubTitulacoes.jasper"));
-		parametros.put("SUB_DEPENDENTE", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubDependente.jasper"));
-		parametros.put("SUB_EMPREGO", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubEmpregos.jasper"));
-		parametros.put("SUB_CONJUGE", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubConjuge.jasper"));
-		parametros.put("SUB_FUNCAO", jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubFuncao.jasper"));
-		parametros.put("COD_SERV", servidor.getCodigo().toString());
-		String nomeDoJasper = "/WEB-INF/jasper/historicoServidor.jasper";
-		jasperMB.geraRelatorioPassandoResultSet(parametros,nomeDoJasper);
-		return "";
+		} else {
+			JasperMB jasperMB = new JasperMB();
+			jasperMB.criaConexao();
+			HashMap parametros = new HashMap();
+			parametros.put("BANNER",
+					jasperMB.getDiretorioReal("/images/banner_topo.gif"));
+			parametros
+					.put("SUB_PESSOAIS",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubPessoais.jasper"));
+			parametros
+					.put("SUB_FUNCIONAL",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubDadosFuncionais.jasper"));
+			parametros
+					.put("SUB_ENDERECO",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubContatoEndereco.jasper"));
+			parametros
+					.put("SUB_DOCUMENTOS",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubDocumentos.jasper"));
+			parametros
+					.put("SUB_CONTA_BANCARIA",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubContaBancaria.jasper"));
+			parametros
+					.put("SUB_TITULACOES",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubTitulacoes.jasper"));
+			parametros
+					.put("SUB_DEPENDENTE",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubDependente.jasper"));
+			parametros
+					.put("SUB_EMPREGO",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubEmpregos.jasper"));
+			parametros
+					.put("SUB_CONJUGE",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubConjuge.jasper"));
+			parametros
+					.put("SUB_FUNCAO",
+							jasperMB.getDiretorioReal("/WEB-INF/jasper/historicoServidor_SubFuncao.jasper"));
+			parametros.put("COD_SERV", servidor.getCodigo().toString());
+			String nomeDoJasper = "/WEB-INF/jasper/historicoServidor.jasper";
+			jasperMB.geraRelatorioPassandoResultSet(parametros, nomeDoJasper);
+			return "";
 		}
 		servidor = new Servidor();
 		return null;
 	}
-	
+
 }
