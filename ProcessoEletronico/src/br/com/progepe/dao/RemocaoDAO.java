2b4b2c61fca355d4c794b8ddaf5798cbb663dd34
processoeletronico.ifpr <processoeletronico.ifpr@139865b2-675c-e321-0b12-2b09a3ca379a>
2011-06-07 13:31:21 +0000

    ####### MANUTENÇÃO ########

---
+++ ProcessoEletronico/src/br/com/progepe/dao/RemocaoDAO.java
@@ -0,0 +1,44 @@
+package br.com.progepe.dao;
+
+import java.util.List;
+
+import org.hibernate.Query;
+
+import br.com.progepe.entity.Remocao;
+
+public class RemocaoDAO extends DAO {
+
+	private static RemocaoDAO instance;
+
+	private RemocaoDAO() {
+	}
+
+	public static RemocaoDAO getInstance() {
+		if (instance == null) {
+			instance = new RemocaoDAO();
+		}
+		return instance;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Remocao> listByFilter(Remocao remocao) {
+		HibernateUtility.getSession().clear();
+		HibernateUtility.beginTransaction();
+		String sql = "from Remocao fs LEFT JOIN FETCH fs.servidor s where 1 = 1 ";
+		if (remocao.getServidor() != null
+				&& remocao.getServidor().getSiape() != null
+				&& remocao.getServidor().getSiape() != 0) {
+			sql += " and s.siape = " + remocao.getServidor().getSiape();
+		}
+		if (remocao.getServidor() != null
+				&& remocao.getServidor().getNome() != null
+				&& remocao.getServidor().getNome() != "") {
+			sql += " and UPPER(s.serv_nome) like '%"
+					+ remocao.getServidor().getNome().toUpperCase() + "%'";
+		}
+		Query query = HibernateUtility.getSession().createQuery(sql);
+		HibernateUtility.commitTransaction();
+		return (List<Remocao>) query.list();
+	}
+
+}
